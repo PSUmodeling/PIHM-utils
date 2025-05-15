533686156052a154dc6b4cc5859f4000674e1fed
Yuning Shi <shiyuning@gmail.com>
2025-05-15 11:59:49 -0400

    :bookmark: v2.0.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PIHM-utils",
-    version="2.0.0",
+    version="2.0.1",
     author="Yuning Shi",
     author_email="shiyuning@gmail.com",
     packages=["pihm"],
